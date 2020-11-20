8382b8e3e1a95922744d18088036062afe39ee95
ibrahim <ibrahimadedayo@rocketmail.com>
2020-11-20 16:11:35 +0100

    Added file to svelte poll project

---
+++ src/App.svelte
@@ -0,0 +1,31 @@
+<script>
+	export let name;
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
+	import Tabs from './shared/Tabs.svelte';
+	
+	// tabs
+	let items = ['Current Polls', 'Add New Poll'];
+	let activeItem = 'Current Polls';
+	const tabChange = (e) => activeItem = e.detail;
+</script>
+
+<Header />
+<main>
+	<Tabs {activeItem} {items} on:tabChange={tabChange} />
+	{#if activeItem === 'Current Polls'}
+		<p>poll list goes here</p>
+	{:else if activeItem === 'Add New Poll'}
+		<CreatePollForm />
+	{/if}
+</main>
+<Footer />
+
+<style>
+	main{
+		width: 100%;
+		max-width: 960px;
+		margin: 40px auto;
+	}
+</style>
